89ee445455c45e9b463f3e6e6f43c1c75a4a1da3
Marissol Karczeski <marfkar@gmail.com>
2020-03-17 16:04:03 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (devoured, burger_name) 
+VALUES (true, 'Cheeseburger');
+
+INSERT INTO burger (devoured, burger_name) 
+VALUES (false, 'Hamburger');
+
+INSERT INTO burger (devoured, burger_name) 
+VALUES (true, 'Chili Burger');
